46912f6df33c8c5f93f316f733319c02b00bae61
ysl19 <yoonislimbo@gamil.com>
2026-05-12 13:16:51 -0500

    fixing component

--- src/components/HourlyForecast.tsx
+++ src/components/HourlyForecast.tsx
@@ -31,43 +31,28 @@ const HourlyForecast = ({ data }: HourlyForecastProps) => {
         <div>Scroll</div>
       </div>
 
-<<<<<<< HEAD:src/components/HourlyForecast.jsx
-      {/* Scrollable list */}
-      <div className="flex flex-col gap-y-2 ">
-        {next24Hours.map((hour, index) => (
+      {next24Hours.map((hour, index) => {
+        const actualIndex = startIndex + index;
+
+        const formattedHour = new Date(hour).toLocaleTimeString("en-US", {
+          hour: "numeric",
+          hour12: true,
+        });
+
+        return (
           <div
             key={hour}
-            className="flex justify-between px-2 py-2 h-12 border rounded-md bg-[#2f2f49] text-white justify-items-center"
+            className="flex justify-between p-3 border rounded-lg"
           >
             <div>
-              <span className="px-2">img </span>
-              <span>{new Date(hour).toLocaleTimeString("en-US", { hour: "numeric", hour12: true })}</span>
-=======
-        {next24Hours.map((hour, index) => {
-          const actualIndex = startIndex + index;
-
-          const formattedHour = new Date(hour).toLocaleTimeString("en-US", {
-            hour: "numeric",
-            hour12: true,
-          });
-
-          return (
-            <div
-              key={hour}
-              className="flex justify-between p-3 border rounded-lg"
-            >
-              <div>
-                <span>img </span>
-                <span>{formattedHour}</span>
-              </div>
-
-              <span>{temperature_2m[actualIndex]}°</span>
->>>>>>> 6cbe939 (convert to tsx):src/components/HourlyForecast.tsx
+              <span>img </span>
+              <span>{formattedHour}</span>
             </div>
-            <span className="px-2">{temperature_2m[startIndex + index]}°</span>
+
+            <span>{temperature_2m[actualIndex]}°</span>
           </div>
-        ))}
-      </div>
+        );
+      })}
     </div>
   );
 };
